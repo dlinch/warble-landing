a90dce08d275a4439ae0808598a0adad8cd154cc
dlinch <dlinch33@gmail.com>
2018-01-15 21:55:23 -0700

    layouts and global nav

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,61 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+// import Header from '../components/Header'
+// import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+// const TemplateWrapper = ({ children }) => (
+//   <div>
+//     <Helmet
+//       title="Gatsby Default Starter"
+//       meta={[
+//         { name: 'description', content: 'Sample' },
+//         { name: 'keywords', content: 'sample, something' },
+//       ]}
+//     />
+//     <Header />
+//     <div
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 960,
+//         padding: '0px 1.0875rem 1.45rem',
+//         paddingTop: 0,
+//       }}
+//     >
+//       {children()}
+//     </div>
+//   </div>
+// )
+
+// TemplateWrapper.propTypes = {
+//   children: PropTypes.func,
+// }
+
+// export default TemplateWrapper
+
+import React from 'react';
+import Link from 'gatsby-link';
+
+const ListLink = props => 
+  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+export default ({children}) => (
+  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
+    {children()}
   </div>
 )
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
-
-export default TemplateWrapper
---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>I'd love to talk to you! Email me at the address below:</h1>
+    <a href="mailto:dlinch33@gmail.com">dlinch33@gmail.com</a>
+  </div>
+)
