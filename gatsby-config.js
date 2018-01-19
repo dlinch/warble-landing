8814dca5d5dfcb0e850546cc559799c6ea6d812a
Derik Linch <dlinch33@gmail.com>
2018-01-19 14:46:18 -0700

    added a source and transformer plugin, index page shows  markdown excerpts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-glamor',
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js',
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
       }
     }
   ],
